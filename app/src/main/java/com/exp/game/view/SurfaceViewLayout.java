4df69613c4d51f431eff6cb589166a1996c6fa1d
cxg4586 <38442648+cxg4586@users.noreply.github.com>
2018-05-07 17:49:01 +1200

    Update SurfaceViewLayout.java

--- app/src/main/java/com/exp/game/view/SurfaceViewLayout.java
+++ app/src/main/java/com/exp/game/view/SurfaceViewLayout.java
@@ -28,77 +28,61 @@ public class SurfaceViewLayout extends View {
 
     private ActionListener actionListener;
 
-    //当前视图(GameLayout)的长和宽
+    //Gamelayout
     private int mLayoutWidth;
     private int mLayoutHeight;
-    //辅助绘制障碍物的对象
     private Barrier mBarrier;
-    //辅助绘制人物的对象
     private Person mPerson;
-    //面板绘制的对象
     private Score mScore;
-
     private Paint mPaint;
-    //小人的圆形半径
     private int radius = 65;
-    //不断绘制的线程
     private Thread mThread;
 
     private MyHandler myHandler;
     private int mBarrierMoveSpeed = 5;
-    //人物是否自动下落状态
     private boolean isAutoFall;
-    //游戏是否正在运行
     private boolean isRunning;
-    //人物左右移动的速度
     private int mPersonMoveSpeed = 20;
 
-    //画面中障碍物的位置信息
     private ArrayList<Barrier> mBarriers;
     private ArrayList<Integer> mBarrierXs;
     private ArrayList<Integer> mBarrierYs;
-    //障碍物起始和产生障碍的间隔
     private int mBarrierStartY = 500;
     private int mBarrierInterval = 500;
-    //障碍物的高度
     private int mBarrierHeight = 60;
-    //人物所站立的障碍在画面中的index
+    
     private int mTouchIndex = -1;
 
-    //当小人自动下落瞬间，开始计时，单位毫秒
     private float mFallTime = 0;
 
     private float mPreSocre = 0;
 
-    //重力加速度
     public static final float G = 9.8f;
 
-    //总得分
     public int mTotalScore;
-    //份数版块的文字大小
+
     private int mTextSize = 17;
 
-    //失败后，弹出的菜单，按钮的位置
+  
     private RectF mRestartRectf;
     private RectF mQuiteRectf;
-    //按钮的宽度和高度，这里我省事没有转化为DP，都是直接用px，所以可能会
-    //产生适配上的问题。
+    
     private int mButtonWidth = 300;
     private int mButtonHeight = 120;
     private int Padding = 20;
 
-    // 血条
+   
     public static final int BLOOD_ALL = 100;
-    // 顶部掉血
+   
     private static final int BLOOD_TOP_SUB = (int) (BLOOD_ALL * 0.8f);
-    // 带刺平台掉血
+   
     private static final int BLOOD_FOOT_BOARD_SUB = 10;
-    // 普通平台加血
+   
     private static final int BLOOD_FOOT_BOARD_ADD = 3;
 
     public static final int TOP_BAR_JUDGE = 100;
 
-    //血条图片
+  
     private Drawable mHpBarTotalImage;
     private Drawable mHpBarRemainImage;
 
@@ -123,22 +107,22 @@ public class SurfaceViewLayout extends View {
     }
 
     private void init() {
-        //初始化画笔
+      
         mPaint = new Paint();
         mPaint.setAntiAlias(true);
         mPaint.setColor(Color.GRAY);
         mPaint.setStrokeWidth(10);
-        //默认开始自动下落
+      
         isAutoFall = true;
         myHandler = new MyHandler();
         mBarriers = new ArrayList<>();
-        //用来记录画面中，每一个障碍物的x坐标
+        
         mBarrierXs = new ArrayList<>();
-        //和上面的x对应的每个障碍物的y坐标
+        
         mBarrierYs = new ArrayList<>();
-        //将文字大小转化成DP
+        
         mTextSize = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_SP, mTextSize, getResources().getDisplayMetrics());
-        //启动游戏
+        
         isRunning = true;
         mHpBarTotalImage = getResources().getDrawable(R.drawable.hp_bar_total);
         mHpBarRemainImage = getResources().getDrawable(R.drawable.hp_bar_remain);
@@ -149,27 +133,26 @@ public class SurfaceViewLayout extends View {
     @Override
     protected void onSizeChanged(int w, int h, int oldw, int oldh) {
         super.onSizeChanged(w, h, oldw, oldh);
-        //当前方法，是在onMeasure调用之后，进行回调，所以直接getMeasureWidth等
-        //获取当前视图的宽和高
+        
         mLayoutWidth = getMeasuredWidth();
         mLayoutHeight = getMeasuredHeight();
         mScreenAttribute = new ScreenAttribute(0, 20, mLayoutWidth, mLayoutHeight);
-        //根据视图宽高，初始化障碍物的信息
+        
         mBarrier = new Barrier(getContext(), mLayoutWidth, mPaint);
         mBarrier.setHeight(mBarrierHeight);
-        //创建人物绘制类对象
+      
         mPerson = new Person(getContext(), mPaint, radius);
         mPerson.mPersonY = 300;
         mPerson.mPersonX = mLayoutWidth / 2;
-        //初始化分数绘制对象
+        
         mScore = new Score(mPaint);
         mScore.x = mLayoutWidth / 2 - mScore.panelWidth / 2;
 
-        //菜单上重启按钮的左边坐标,mRestartRectf是重启按钮绘制区域
+        
         int rX = mLayoutWidth / 2 - 20 - mButtonWidth;
         int rY = mLayoutHeight * 3 / 5;
         mRestartRectf = new RectF(rX, rY, rX + mButtonWidth, rY + mButtonHeight);
-        //下面是菜单上退出按钮的区域
+     
         int qX = mLayoutWidth / 2 + 20;
         int qY = mLayoutHeight * 3 / 5;
         mQuiteRectf = new RectF(qX, qY, qX + mButtonWidth, qY + mButtonHeight);
@@ -179,13 +162,13 @@ public class SurfaceViewLayout extends View {
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
         if ((mTotalScore - mPreSocre) > SPEED_ADD_SCORE) {
-            //加快速度，每10秒加快一次
+           
             mPreSocre = mTotalScore;
             mBarrierMoveSpeed += SPEED_ADD;
             postInvalidate();
             return;
         }
-        //判断是否到达顶部并且掉血后仍然存活
+     
         if (topAndSubBlood()) {
             mPerson.blood -= BLOOD_TOP_SUB;
             mPerson.mPersonY += mBarrierHeight * 2;
@@ -193,24 +176,24 @@ public class SurfaceViewLayout extends View {
             postInvalidate();
             return;
         }
-        // 绘制顶部刺
+        
         generateTop(canvas);
-        //绘制障碍物
+       
         generateBarrier(canvas);
-        //如果小人正在下落，才检测是否碰撞
+        
         if (isAutoFall)
             checkTouch();
-        //根据是否下落，绘制小人的位置
+        
         generatePerson(canvas);
-        //检查小人是否超出边界以及血量，判断游戏是否结束
+       
         isRunning = !checkIsGameOver();
-        //如果游戏结束
+        
         if (!isRunning) {
             if(actionListener != null){
                 actionListener.over();
             }
         }
-        //绘制血条
+
         mHpBarTotalImage.setBounds(
                 (this.mScreenAttribute.maxX / 9),
                 this.mScreenAttribute.maxY - 65,
@@ -224,7 +207,7 @@ public class SurfaceViewLayout extends View {
                         (int) (this.mScreenAttribute.maxX * 8 / 9 * (float) (mPerson.blood * 1.f / BLOOD_ALL)),
                         this.mScreenAttribute.maxY - 40);
         mHpBarRemainImage.draw(canvas);
-        //绘制分数面板
+     
         generateScore(canvas);
     }
 
@@ -234,9 +217,9 @@ public class SurfaceViewLayout extends View {
     }
 
     /**
-     * 绘制结束弹出框的背景区域
+     * 
      *
-     * @param canvas
+     * 
      */
     private void drawPanel(Canvas canvas) {
         mPaint.setStyle(Paint.Style.FILL_AND_STROKE);
@@ -245,9 +228,7 @@ public class SurfaceViewLayout extends View {
     }
 
     /**
-     * 绘制Game over文字
      *
-     * @param canvas
      */
     private void notifyGameOver(Canvas canvas) {
         mPaint.setTextAlign(Paint.Align.CENTER);
@@ -257,7 +238,7 @@ public class SurfaceViewLayout extends View {
         canvas.drawText("Game over", mLayoutWidth / 2, mLayoutHeight / 2, mPaint);
     }
 
-    //绘制菜单按钮,下面的操作使得文字能够居中显示
+    
     private void drawButton(Canvas canvas, RectF rectF, String text, int strokeColor, int textColor) {
         mPaint.setStyle(Paint.Style.FILL);
         mPaint.setColor(strokeColor);
@@ -273,9 +254,9 @@ public class SurfaceViewLayout extends View {
     }
 
     /**
-     * 绘制分数面板
+     * 
      *
-     * @param canvas
+     * 
      */
     private void generateScore(Canvas canvas) {
         mPaint.setStyle(Paint.Style.FILL);
@@ -288,31 +269,25 @@ public class SurfaceViewLayout extends View {
     }
 
     /**
-     * 据初始位置，生成障碍物,难点
-     * 1.绘制时，每一个障碍物间的距离是一致的
-     * 2.绘制时，都是从第一个障碍物开始绘制
-     * 3.循环绘制，并把障碍物的x，y位置，分别保存在数组中
-     * 4.障碍物逐渐上升，当障碍物超出边界时，我们删除数组中保存的
-     * 第一个位置的x，但是保持原有下面已经出现过得障碍物x的位置
-     * 并在最后添加新的障碍物的位置；y位置，每次都重新生成，重新
-     * 保存在数组中
+     * 
+     * 
+     * 
      */
     private void generateBarrier(Canvas canvas) {
         mPaint.setStyle(Paint.Style.FILL);
         mPaint.setColor(Color.DKGRAY);
-        //每次都清楚Y坐标信息，因为后面会重新生成
+      
         mBarrierYs.clear();
-        //死循环，有条件退出
+        
         for (int i = 0; ; ) {
-            //i小于数组中的长度，那么取出原有的x位置信息，绘制旧障碍物;
-            // 否则就随机生成新的坐标信息添加到数组中
+            
             if (i < mBarrierXs.size()) {
                 mBarrier.mPositionX = mBarrierXs.get(i);
             } else {
                 mBarrier.mPositionX = RandomUtil.getRangeX(mLayoutWidth);
                 mBarrierXs.add(mBarrier.mPositionX);
             }
-            //障碍物的y坐标
+           
             mBarrier.mPositionY = mBarrierStartY + mBarrierInterval * i;
             mBarrierYs.add(mBarrier.mPositionY);
             Barrier b = null;
@@ -328,7 +303,7 @@ public class SurfaceViewLayout extends View {
                 b.setType(RandomUtil.getRandomType());
                 mBarriers.add(b);
             }
-            //绘制到视图外，则不再进行绘制，退出循环
+            
             if (mBarrier.mPositionY > mLayoutHeight) {
                 break;
             }
@@ -341,37 +316,36 @@ public class SurfaceViewLayout extends View {
     public static final int SPEED_ADD_SCORE = 20;
 
     private void generatePerson(Canvas canvas) {
-        //如果小人在自动下落
+        
         if (isAutoFall) {
-            //自动下落绘制
+          
             mFallTime += 20;
-            //根据重力加速度计算小人下落的位置
+            
             mPerson.mPersonY += mFallTime / 1000 * G;
             mPerson.draw(canvas);
         } else {
-            //获取被挡住的障碍位置
+            
             Log.v("@time", mFallTime / 1000 + "");
-            //小人被挡住，下落的时间重置
+            
             mFallTime = 0;
-            //mTouchIndex表示的是小人在视图中被阻挡的的障碍物的位置
-            //如果是小于0,表示没有阻挡,
+            
             if (mTouchIndex >= 0) {
-                //设置小人被阻挡的位置，被进行绘制
+               
                 mPerson.mPersonY = mBarrierYs.get(mTouchIndex) - 2 * radius;
                 mPerson.draw(canvas);
             }
         }
     }
 
-    // 记录上次落地的Barrier
+  
     private Barrier preSubBarrier;
 
     /**
-     * 碰撞检测
+     *
      */
     private void checkTouch() {
         for (int i = 0; i < Math.min(mBarrierXs.size(), mBarrierYs.size()); i++) {
-            //碰撞检测
+            
             if (isTouchBarrier(mBarrierXs.get(i), mBarrierYs.get(i))) {
                 mTouchIndex = i;
                 isAutoFall = false;
@@ -400,18 +374,14 @@ public class SurfaceViewLayout extends View {
     }
 
     /**
-     * 碰撞检测
      *
-     * @param x 障碍物x坐标
-     * @param y 障碍物y坐标
-     * @return
+     *
+     * 
      */
     private boolean isTouchBarrier(int x, int y) {
         boolean res = false;
         int pY = mPerson.mPersonY + 2 * radius;
-        //在瞬间刷新的时候，只要小人的位置和障碍的位置，差值在小人和障碍物的瞬间刷新的最大值就属于碰撞
-        //比如：小人下落速度为a,障碍物上升速度为b,画面刷新时间为t,瞬间刷新，会有个最大差值，这个值就是
-        //临界值
+        
         if (Math.abs(pY - y) <= Math.abs(mBarrierMoveSpeed + Person.SPEED + mFallTime / 1000 * G)) {
             if (mPerson.mPersonX + 2 * radius >= x && mPerson.mPersonX <= x + mBarrier.getWidth()) {
                 res = true;
@@ -438,25 +408,25 @@ public class SurfaceViewLayout extends View {
                     if(pause){
                         continue;
                     }
-                    //开始让障碍往上面滚动,障碍物的绘制，是跟mBarrierStartY相关的
+                    
                     mBarrierStartY -= mBarrierMoveSpeed;
-                    //当第一个障碍物开始消失
+                   
                     if (mBarrierStartY <= -mBarrierInterval - mBarrierHeight) {
                         mBarrierStartY = -mBarrierHeight;
-                        //删除刚消失的障碍物坐标信息
+                        
                         if (mBarrierXs.size() > 0) {
                             mBarrierXs.remove(0);
                             mBarriers.remove(0);
                         }
-                        //得分++
+                       
                         mTotalScore++;
-                        //小球碰撞位置--
+                        
                         mTouchIndex--;
                     }
-                    //这里应该是可以直接用postInvalidate()
+                   
                     myHandler.sendEmptyMessage(0x1);
                     try {
-                        //每20毫秒刷新一次界面
+                      
                         Thread.sleep(16);
                     } catch (InterruptedException e) {
                         e.printStackTrace();
@@ -478,12 +448,12 @@ public class SurfaceViewLayout extends View {
         }
     }
 
-    //绘制正常小人
+   
     public void persongNormal() {
         mPerson.type = 0;
     }
 
-    //控制小人向左移动
+   
     public void moveLeft() {
         if (!isRunning) {
             return;
@@ -494,12 +464,12 @@ public class SurfaceViewLayout extends View {
         if (dir < 0)
             dir = 0;
         mPerson.mPersonX = dir;
-        //移动过程中，启动边界检测,设置isAutoFall为true
+       
         checkIsOutSide(dir);
     }
 
     /**
-     * 类似moveLeft
+     *
      */
     public void moveRight() {
         if (!isRunning) {
@@ -516,7 +486,7 @@ public class SurfaceViewLayout extends View {
 
     private void checkIsOutSide(int x) {
         if (preSubBarrier != null) {
-            //碰撞检测
+           
             if (isTouchBarrier(preSubBarrier.mPositionX, preSubBarrier.mPositionY)) {
                 isAutoFall = false;
             } else {
@@ -530,7 +500,7 @@ public class SurfaceViewLayout extends View {
     }
 
     /**
-     * 重置游戏信息
+     * 
      */
     public void restartGame() {
         mBarrierXs.clear();
